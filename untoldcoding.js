773e97bda9b0292cf891594d1a8d39d19b4bfa22
PrimeX <103210935+DevPrimeX@users.noreply.github.com>
2024-01-14 19:02:55 +0530

    Update untoldcoding.js

--- untoldcoding.js
+++ untoldcoding.js
@@ -1,8 +1,10 @@
 var i = 0;
 var txt1 =
   "Hi ....!  <<               Now I wan't to say something special to you. <<I believe you might have already figured out <<<who I am, and perhaps even blocked me. I created this site to express things > << I might find difficult to say face to face. It's amazing how, 1.5 years ago, we knew each other and talked, but now it feels like we're strangers. > << I acknowledge my mistakes, and I apologize, but you also have a role in this.> << You once said I'm good-hearted, then called me bad. << I accepted it, but it would have been better if you discussed it with me first.> <<If you haven't blocked me yet, delete the message from the number that contacted you.> << After your exam, we can talk. By the way, if you've forgiven me for that, let me know.>"
-  var speed = 80;
+var speed = 80;
+
 typeWriter();
+
 function typeWriter() {
   if (i < txt1.length) {
     if (txt1.charAt(i) == "<")
@@ -14,5 +16,15 @@ function typeWriter() {
     } else document.getElementById("text1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
+  } else {
+    // Create a button element
+    var button = document.createElement("button");
+    button.innerHTML = "Click me!";
+    // Set an onclick function to redirect when clicked
+    button.onclick = function() {
+      window.location.href = "https://example.com"; // Replace with your desired link
+    };
+    // Append the button to the body or any other container
+    document.body.appendChild(button);
   }
 }
