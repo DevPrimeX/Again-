6c73254de0a5d66496bcbbfd35463bbcb8f53135
PrimeX <103210935+DevPrimeX@users.noreply.github.com>
2024-01-14 18:36:21 +0530

    Update untoldcoding.js

--- untoldcoding.js
+++ untoldcoding.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt1 =
   "Hi ....!  <<               Now I wan't to say something special to you. <<Hi there, I believe you might have already figured out <<<who I am, and perhaps even blocked me. I created this site to express things > <<<I might find difficult to say face to face. It's amazing how, 1.5 years ago, we knew each other and talked, but now it feels like we're strangers. ><<<I acknowledge my mistakes, and I apologize, but you also have a role in this.> <<<You once said I'm good-hearted, then called me bad. << I accepted it, but it would have been better if you discussed it with me first.><<<If you haven't blocked me yet, delete the message from the number that contacted you.><<After your exam, we can talk. By the way, if you've forgiven me for that, let me know.>"
-  var speed = 55;
+  var speed = 100;
 typeWriter();
 function typeWriter() {
   if (i < txt1.length) {
